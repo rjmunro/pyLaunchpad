5e5f292cab073827430af9ac5f725d2bade97cf9
Robert (Jamie) Munro <rjmunro@arjam.net>
2011-11-19 20:36:40 +0000

    Support top row (y=8)

--- launchpad.py
+++ launchpad.py
@@ -45,9 +45,17 @@ class launchpad:
 
 	def light(self, x, y, red, green):
 		if not 0 <= x <= 8: raise LaunchPadError("Bad x value %s" % x)
-		if not 0 <= y <= 7: raise LaunchPadError("Bad y value %s" % y)
+		if not 0 <= y <= 8: raise LaunchPadError("Bad y value %s" % y)
 		if not 0 <= red <= 3: raise LaunchPadError("Bad red value %s" % red)
 		if not 0 <= green <= 3: raise LaunchPadError("Bad green value %s" % green)
+
+		velocity = 16*green + red + 8 + 4
+
+		if y==8:
+			if x != 8:
+				note = 104 + x
+				self.midiOut.WriteShort(0xb0,note,velocity)
+			return
 
 		if self.drumrackMode:
 			if x==8:
@@ -60,7 +68,7 @@ class launchpad:
 				note = 64 + x + 4*y
 		else:
 			note = x + 16*(7-y)
-		velocity = 16*green + red + 8 + 4
+
 		self.midiOut.WriteShort(0x90,note,velocity)
 
 if __name__=="__main__":

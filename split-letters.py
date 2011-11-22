4d7f7af905fdbee22bf0103935023c50452b7690
Robert (Jamie) Munro <rjmunro@arjam.net>
2011-11-22 21:46:17 +0000

    Split text string image into individual images.

---
+++ split-letters.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python2.7
+import time
+import sys
+
+if __name__=="__main__":
+
+	import Image
+	im = Image.open(sys.argv[1])
+	xsize,ysize = im.size
+
+	letters = list("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789")
+
+	startx = 0
+	for x in range(xsize):
+		split = True
+		for y in range(ysize):
+			r,g,b = im.getpixel((x,8-y))
+			if r or g or b:
+				split = False
+				break
+		if split:
+			if x > (startx + 1):
+				letter = letters.pop(0)
+				print startx, x
+				im.crop((startx+1,0,x,8)).save('letters/' + letter + '.png')
+			startx = x
+
+

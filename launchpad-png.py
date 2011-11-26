d3d3d5e1aa3b79b2a3157318f2c3bb8f543e7dbb
Robert (Jamie) Munro <rjmunro@arjam.net>
2011-11-26 17:57:09 +0000

    Refactored inline code into showImage function and added offsety option.

--- launchpad-png.py
+++ launchpad-png.py
@@ -2,6 +2,16 @@
 import launchpad
 import time
 import sys
+import Image
+
+def showImage(im, offsetx=0, offsety=0):
+	grid = []
+	for x in range(9):
+		grid.append([])
+		for y in range(9):
+			r,g,b = im.getpixel((x + offsetx, 8-y + offsety))
+			grid[x].append((r/64, g/64))
+	l.lightAll(grid)
 
 if __name__=="__main__":
 	launchPads = launchpad.findLaunchpads()
@@ -10,20 +20,12 @@ if __name__=="__main__":
 	l.reset()
 	l.setDrumRackMode()
 
-	import Image
 	im = Image.open(sys.argv[1])
 	xsize,ysize = im.size
 
     
 	for startx in range(xsize-8):
-		grid = []
-		for x in range(9):
-			grid.append([])
-			for y in range(9):
-				r,g,b = im.getpixel((x+startx,8-y))
-				grid[x].append((r/64, g/64))
-
-		l.lightAll(grid)
+		showImage(im, startx)
 		time.sleep(.1)
 
 	time.sleep(1)

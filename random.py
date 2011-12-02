f992e0facf1cf07d0f8ece9fe5a720f7aefd84a7
Robert (Jamie) Munro <rjmunro@arjam.net>
2011-12-02 14:09:14 +0000

    Quick test that finds all launchpads and lights them at random.

---
+++ random.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python2.7
+import launchpad
+import time
+import random
+
+if __name__=="__main__":
+	launchPads = launchpad.findLaunchpads()
+	launchPadObjs = []
+	for l in launchPads:
+		launchPadObjs.append(launchpad.launchpad(*l))
+
+	#l.reset()
+	#l.ledTest(1)
+	#l.setDrumRackMode()
+
+	while 1:
+		for l in launchPadObjs:
+			x = random.randint(0, 8)
+			y = random.randint(0, 8)
+			r = random.randint(0, 3)
+			g = random.randint(0, 3)
+
+			l.light(x,y,r,g)

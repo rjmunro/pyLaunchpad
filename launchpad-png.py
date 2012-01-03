ec330c9aba7932a89b2a693a5ac77b815826b360
Robert (Jamie) Munro <rjmunro@arjam.net>
2012-01-03 09:34:16 +0000

    Move core of launchpad-png into a function drawImage that can be imported.

--- launchpad-png.py
+++ launchpad-png.py
@@ -4,14 +4,14 @@ import time
 import sys
 import Image
 
-if __name__=="__main__":
-	launchPads = launchpad.findLaunchpads()
-	launchPadObjs = []
-	for l in launchPads:
-		launchPadObjs.append(launchpad.launchpad(*l))
-		launchPadObjs[-1].reset()
+# Set up launchpads
+launchPads = launchpad.findLaunchpads()
+launchPadObjs = []
+for l in launchPads:
+	launchPadObjs.append(launchpad.launchpad(*l))
+	launchPadObjs[-1].reset()
 
-	im = Image.open(sys.argv[1])
+def drawImage(im):
 	xsize,ysize = im.size
 
 	for startx in range(max(xsize-(11*len(launchPads)/2)+3,1)):
@@ -20,6 +20,12 @@ if __name__=="__main__":
 				l = launchPadObjs[padx*2+pady]
 				l.showImage(im, 11*padx + startx, 10*pady)
 		time.sleep(.1)
+
+if __name__=="__main__":
+
+	im = Image.open(sys.argv[1])
+
+	drawImage(im)
 
 	# Sleep to allow buffer to empty
 	time.sleep(1)

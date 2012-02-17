58efd35e2ce554a74c1e2f966e68cc595896a11d
Robert (Jamie) Munro <rjmunro@arjam.net>
2012-02-17 11:28:42 +0000

    Have runEffect return the last frame.

--- runEffects.py
+++ runEffects.py
@@ -15,13 +15,15 @@ def runEffect(ef, count = 10000):
 		launchpads.drawImage(im.crop((0,0)+launchpads.getTotalSize()))
 		launchpads.poll()
 		count -= 1
+	return im
 
 if __name__ == "__main__":
 	from bars import vuBarsEffect
 	from fire import fireEffect
 	from scroll import scrollImage, scrollText
 	while 1:
-		runEffect(scrollText("Novation Launchpad is a 64 button 'music controller' that enables you to make music or mix tracks by other artists. You don't need advanced musical knowledge, just your own creativity."))
-		runEffect(scrollImage(Image.open("images/novation-launchpad-2line.png")))
-		runEffect(vuBarsEffect(),100)
-		runEffect(fireEffect(Image.new('RGB',(31,20))),100)
+		im = Image.new('RGB',(32,20))
+		im = runEffect(scrollText("Novation Launchpad is a 64 button 'music controller' that enables you to make music or mix tracks by other artists. You don't need advanced musical knowledge, just your own creativity."),100)
+		im = runEffect(scrollImage(Image.open("images/novation-launchpad-2line.png")))
+		im = runEffect(vuBarsEffect(),100)
+		im = runEffect(fireEffect(im),100)

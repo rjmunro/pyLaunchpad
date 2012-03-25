2cac4a53eb8a5094ba17e157412bea5c2eeba4d3
Robert (Jamie) Munro <rjmunro@arjam.net>
2012-03-25 21:31:34 +0100

    Genericised vuBarsEffect to take parameters for width etc. rather than be fixed to old layout.

--- bars.py
+++ bars.py
@@ -6,25 +6,25 @@ import ImageDraw
 # Amount of frames to spend going to destination before new random destination is picked.
 STEPS = 8
 
-def vuBarsEffect():
-	levels = [random.randint(0,8) for i in range(9)]
-	im = Image.new('RGB',(31,20))
+def vuBarsEffect(width=2, height=9, space=1, number=3):
+	levels = [random.randint(0,height) for i in range(number)]
+	im = Image.new('RGB',((width+space)*number,height))
 	draw = ImageDraw.Draw(im)
 	while 1:
-		newLevels = [random.randint(0,8) for i in range(9)]
+		newLevels = [random.randint(0,height) for i in range(number)]
 		for step in range(STEPS):
 			# Clear image
-			draw.rectangle((0,0,33,20),fill=(0,0,0))
+			draw.rectangle((0,0)+im.size,fill=(0,0,0))
 			averageLevels = [ a + ((b-a)*step)/STEPS for [a,b] in zip(levels,newLevels)]
 			for i in range(len(averageLevels)):
-				x1 = i*3 + i/3*2
-				x2 = x1+1
-				y2 = 20
+				x1 = i*(width + space)
+				x2 = x1 + width - 1
+				y2 = height
 				y1 = y2-averageLevels[i]
 				# Draw green bar
 				draw.rectangle((x1,y1,x2,y2),fill=(0,255,0))
-				if (y1<14):
+				if (y1<2):
 					# Draw red peak
-					draw.rectangle((x1,y1,x2,13),fill=(255,0,0))
+					draw.rectangle((x1,y1,x2,1),fill=(255,0,0))
 			yield im
 		levels = newLevels

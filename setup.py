7d815d0df2d7012d87b7e30486caff80897a1f49
Dave Arter <dave@minus-zero.org>
2012-10-31 21:23:16 +0000

    Refactored into setuptools/PyPI-friendly structure

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name = "pylaunchpad",
+    version = "0.0.1",
+    author = "Robert (Jamie) Munro",
+    description = "Python interface for Novation Launchpad",
+    license = "Apache",
+    keywords = "launchpad novation ableton midi",
+    url = "https://github.com/davea/pyLaunchpad",
+    packages = ["pylaunchpad"],
+    classifiers = [
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Multimedia :: Sound/Audio :: MIDI",
+        
+    ],
+)
